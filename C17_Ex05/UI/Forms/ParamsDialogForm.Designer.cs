39eb50c07ce9e0b49f226dd937dfab5cb682973f
Jacob Roginsky <jacobroginsky@gmail.com>
2017-09-22 18:09:18 +0300

    ParamsDialogForm

---
+++ C17_Ex05/UI/Forms/ParamsDialogForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace C17_Ex05.UI.Forms
+{
+    partial class ParamsDialogForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPlayers = new System.Windows.Forms.Label();
+            this.labelPlayer1 = new System.Windows.Forms.Label();
+            this.checkBoxPlayer2 = new System.Windows.Forms.CheckBox();
+            this.textBoxPlayer1 = new System.Windows.Forms.TextBox();
+            this.textBoxPlayer2 = new System.Windows.Forms.TextBox();
+            this.labelBoardSize = new System.Windows.Forms.Label();
+            this.labelRows = new System.Windows.Forms.Label();
+            this.labelCols = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPlayers
+            // 
+            this.labelPlayers.AutoSize = true;
+            this.labelPlayers.Location = new System.Drawing.Point(10, 13);
+            this.labelPlayers.Name = "labelPlayers";
+            this.labelPlayers.Size = new System.Drawing.Size(44, 13);
+            this.labelPlayers.TabIndex = 0;
+            this.labelPlayers.Text = "Players:";
+            // 
+            // labelPlayer1
+            // 
+            this.labelPlayer1.AutoSize = true;
+            this.labelPlayer1.Location = new System.Drawing.Point(24, 34);
+            this.labelPlayer1.Name = "labelPlayer1";
+            this.labelPlayer1.Size = new System.Drawing.Size(48, 13);
+            this.labelPlayer1.TabIndex = 1;
+            this.labelPlayer1.Text = "Player 1:";
+            // 
+            // checkBoxPlayer2
+            // 
+            this.checkBoxPlayer2.AutoSize = true;
+            this.checkBoxPlayer2.Location = new System.Drawing.Point(27, 60);
+            this.checkBoxPlayer2.Name = "checkBoxPlayer2";
+            this.checkBoxPlayer2.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxPlayer2.TabIndex = 2;
+            this.checkBoxPlayer2.Text = "Player 2:";
+            this.checkBoxPlayer2.UseVisualStyleBackColor = true;
+            this.checkBoxPlayer2.CheckedChanged += new System.EventHandler(this.checkBoxPlayer2_CheckedChanged);
+            // 
+            // textBoxPlayer1
+            // 
+            this.textBoxPlayer1.Location = new System.Drawing.Point(96, 34);
+            this.textBoxPlayer1.Name = "textBoxPlayer1";
+            this.textBoxPlayer1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPlayer1.TabIndex = 3;
+            // 
+            // textBoxPlayer2
+            // 
+            this.textBoxPlayer2.Enabled = false;
+            this.textBoxPlayer2.Location = new System.Drawing.Point(96, 60);
+            this.textBoxPlayer2.Name = "textBoxPlayer2";
+            this.textBoxPlayer2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPlayer2.TabIndex = 4;
+            this.textBoxPlayer2.Text = "[Computer]";
+            // 
+            // labelBoardSize
+            // 
+            this.labelBoardSize.AutoSize = true;
+            this.labelBoardSize.Location = new System.Drawing.Point(10, 96);
+            this.labelBoardSize.Name = "labelBoardSize";
+            this.labelBoardSize.Size = new System.Drawing.Size(61, 13);
+            this.labelBoardSize.TabIndex = 5;
+            this.labelBoardSize.Text = "Board Size:";
+            // 
+            // labelRows
+            // 
+            this.labelRows.AutoSize = true;
+            this.labelRows.Location = new System.Drawing.Point(24, 125);
+            this.labelRows.Name = "labelRows";
+            this.labelRows.Size = new System.Drawing.Size(37, 13);
+            this.labelRows.TabIndex = 6;
+            this.labelRows.Text = "Rows:";
+            // 
+            // labelCols
+            // 
+            this.labelCols.AutoSize = true;
+            this.labelCols.Location = new System.Drawing.Point(123, 125);
+            this.labelCols.Name = "labelCols";
+            this.labelCols.Size = new System.Drawing.Size(30, 13);
+            this.labelCols.TabIndex = 7;
+            this.labelCols.Text = "Cols:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(67, 123);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(159, 123);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonStart.Location = new System.Drawing.Point(27, 166);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(169, 23);
+            this.buttonStart.TabIndex = 10;
+            this.buttonStart.Text = "Start!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // ParamsDialogForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 206);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelCols);
+            this.Controls.Add(this.labelRows);
+            this.Controls.Add(this.labelBoardSize);
+            this.Controls.Add(this.textBoxPlayer2);
+            this.Controls.Add(this.textBoxPlayer1);
+            this.Controls.Add(this.checkBoxPlayer2);
+            this.Controls.Add(this.labelPlayer1);
+            this.Controls.Add(this.labelPlayers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ParamsDialogForm";
+            this.Text = "Game Settings";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPlayers;
+        private System.Windows.Forms.Label labelPlayer1;
+        private System.Windows.Forms.CheckBox checkBoxPlayer2;
+        private System.Windows.Forms.TextBox textBoxPlayer1;
+        private System.Windows.Forms.TextBox textBoxPlayer2;
+        private System.Windows.Forms.Label labelBoardSize;
+        private System.Windows.Forms.Label labelRows;
+        private System.Windows.Forms.Label labelCols;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
---
+++ C17_Ex05/UI/Forms/ParamsDialogForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C17_Ex05.UI.Forms
+{
+    public partial class ParamsDialogForm : Form
+    {
+        public ParamsDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxPlayer2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.textBoxPlayer2.Enabled = !this.textBoxPlayer2.Enabled;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.numericUpDown2.Value = this.numericUpDown1.Value;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            this.numericUpDown1.Value = this.numericUpDown2.Value;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
